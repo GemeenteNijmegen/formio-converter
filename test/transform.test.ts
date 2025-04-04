d620fdf976be9abc74ee8b1b5a2715c46f7c945e
Marnix Dessing <m.dessing@nijmegen.nl>
2025-04-04 14:26:52 +0200

    feat: subforms are replaced with a content element to indicate usage of a subform

--- test/transform.test.ts
+++ test/transform.test.ts
@@ -1,6 +1,6 @@
 import { readFileSync } from 'fs';
 import { join } from 'path';
-import { convertHtmlContent, replaceSubform } from '../src/convert';
+import { convertHtmlContent } from '../src/convert';
 import { FormDefinitionTransformer } from '../src/FromDefinitionTransformer';
 
 describe('FormDefinitionTransformer', () => {
@@ -8,11 +8,11 @@ describe('FormDefinitionTransformer', () => {
   const exportFile = readFileSync(join(__dirname, '../', 'testdata', '2024-10-05-ontwikkel.json'), { encoding: 'utf-8' });
   const formDefinitionsExport = JSON.parse(exportFile);
 
-  test('Remove subforms', () => {
-    const testform = formDefinitionsExport.forms.devops;
-    new FormDefinitionTransformer(replaceSubform, { formDefinitionsExport: formDefinitionsExport, output: [] }).transform(testform);
-    expect(JSON.stringify(formDefinitionsExport)).not.toMatch('"key": "nonhiddenfields');
-  });
+  // test('Remove subforms', () => {
+  //   const testform = formDefinitionsExport.forms.devops;
+  //   new FormDefinitionTransformer(replaceSubform, { formDefinitionsExport: formDefinitionsExport, output: [] }).transform(testform);
+  //   expect(JSON.stringify(formDefinitionsExport)).not.toMatch('"key": "nonhiddenfields');
+  // });
 
   test('Convert htmlelements', () => {
     const testform = formDefinitionsExport.forms.individueleInkomenstoeslagAanvragen2;
